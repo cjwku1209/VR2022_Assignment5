03b25aa143fc6583f36c7f10a73f5975741b6262
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2022-03-08 10:45:41 +0800

    tilt works

--- client/src/pages/control-page/control-page.jsx
+++ client/src/pages/control-page/control-page.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { connect } from 'react-redux';
 import { Joystick } from 'react-joystick-component';
+import Tilt from 'react-parallax-tilt';
 
 class Component extends React.Component {
 
@@ -8,41 +9,44 @@ class Component extends React.Component {
 		super(props);
 		this.state = {
             angleX : 0,
-            angelY : 0
+            angleY : 0
 		}
 	}
 
 	render() {
 		return (
 			<React.Fragment>
-                {/* <div style={{
-                    display: 'flex',
-                    alignItems: 'center',
-                    justifyContent: 'center',
-                }}>
-                    <Tilt tiltAngleXManual={this.state.angleX} tiltAngleYManual={this.state.angelY} glareEnable={true}>
-                    
-                    </Tilt>
-                </div> */}
                 <div style={{
                     display: 'flex',
                     alignItems: 'center',
                     justifyContent: 'center',
+                    marginTop: "20px"
+                }}>
+                    <Tilt tiltAngleXManual={this.state.angleX} tiltAngleYManual={this.state.angleY} glareEnable={false}>
+                        <div style={{ height: '300px', width: '300px', backgroundColor: '#FFD300', borderColor: '#FFFF99'}}>
+                            <div>Axis x: {this.state.angleX}°</div>
+                            <div>Axis y: {this.state.angleY}°</div>
+                        </div>
+                    </Tilt>
+                </div>
+                <div style={{
+                    display: 'flex',
+                    alignItems: 'center',
+                    justifyContent: 'center',
+                    marginTop: "20px"
                 }}>
                     <Joystick 
-                    size={100} 
-                    sticky={true} 
-                    baseColor={"#FFFF99"} 
-                    stickColor={"#FFD300"}
-                    move = {(stick)=>{
-                        console.log(stick.x)
-                        console.log(stick.y)
-                        this.setState({
-                            angleX: stick.x,
-                            angleY: stick.y
-                        })
-                        
-                    }}/>
+                        size={100} 
+                        sticky={true} 
+                        baseColor={"#FFFF99"} 
+                        stickColor={"#FFD300"}
+                        move = {(stick)=>{
+                            this.setState({
+                                angleX: stick.y,
+                                angleY: stick.x
+                            })
+                            
+                        }}/>
                 </div>
 				
 			</React.Fragment>
