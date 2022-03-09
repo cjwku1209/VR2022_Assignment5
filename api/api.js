86625d71648a13cf07f0b67c41490655c7b17e53
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2022-03-09 14:04:59 +0800

    mongodb successful

--- api/api.js
+++ api/api.js
@@ -1,5 +1,5 @@
 import express from 'express';
-
+import {users} from "../models/users";
 export const api = express.Router();
 
 api.get('/api', (request, response) => {
@@ -9,6 +9,63 @@ api.get('/api', (request, response) => {
 		method: stack.route.stack[0].method
 	}))));
 });
+
+api.post('/api/create-user', (request, response)=>{
+	const user = new users({
+		firstName: request.body.firstName,
+		lastName: request.body.lastName,
+		username: request.body.username,
+		password: request.body.password,
+		email: request.body.email
+	});
+	const promise = user.save();
+	promise.then(()=>{
+		response.status(201).json({
+			success: true,
+			message: "Account Created"
+		});
+	}).catch((err)=>{
+		response.status(403).json({
+			success: false,
+			message: err.message
+		});
+	})
+})
+
+api.post('/api/login', (request, response)=>{
+
+	users.findOne({username: request.body.username}).then(function(user) {
+		if(!user) {
+			throw new Error('This username does not exist')
+		}
+		else{
+			user.comparePassword(request.body.password, function(err, isMatch) {
+				if (err) throw err;
+				else{
+					if(isMatch){
+						response.status(201).json({
+							success: true,
+							user: user,
+							_id: user._id,
+							message: "Login Successful"
+						});
+					}
+					else{
+						response.status(400).json({
+							success: false,
+							message: "Wrong Password"
+						});
+					}
+				}
+			});
+		}
+	}).catch(function(err) {
+		response.status(400).json({
+			success: false,
+			message: err.message
+		});
+	})
+})
 
 api.all('/api/*', (request, response) => {
 	response.sendStatus(404);
--- index.js
+++ index.js
@@ -3,6 +3,13 @@ import express from 'express';
 import morgan from 'morgan';
 import { api } from './api/api';
 
+// Setting up Mongodb
+import session from 'express-session';
+const MongoStore = require('connect-mongo');
+import db from './server'
+import {users} from "./models/users";
+
+
 const NODE_ENV = process.env.NODE_ENV || 'development';
 const PORT = process.env.PORT || 8080;
 
@@ -11,6 +18,16 @@ const app = express();
 app.use(express.json());
 app.use(morgan('common'));
 app.use(api);
+
+app.use(session({
+	secret: 'secret',
+	resave: false,
+	saveUninitialized: false,
+	store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/VR2022_HW5'}),
+	cookie: { maxAge: 600 * 1000 },
+}));
+db.on('error', console.error.bind(console, 'MongoDB connection error:'));
+app.use(users);
 
 if (NODE_ENV === 'production') {
 	app.use(express.static(path.join(__dirname, '../client/build')));
