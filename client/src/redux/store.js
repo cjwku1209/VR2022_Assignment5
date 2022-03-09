07e19f35e7ec6e290eb378f88735a4744a458e1b
Calvin Ku <calvinku@gapp.nthu.edu.tw>
2022-03-09 15:38:58 +0800

    login and alert redux complete

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -11,16 +11,21 @@ import {
 } from 'redux-saga/effects';
 import { LoggerReducer } from './logger/logger-reducer';
 import { LoggerSaga } from './logger/logger-saga';
+import {LoginReducer} from "./login/login-reducer";
+import {AlertReducer} from "./alert/alert-reducer";
+import {LoginSaga} from "./login/login-saga";
 
 const saga = createSagaMiddleware();
 
 export const Store = createStore(combineReducers({
-	LoggerReducer
+	LoggerReducer,
+	LoginReducer,
+	AlertReducer,
 }), applyMiddleware(saga));
 
 saga.run(function* () {
 	yield all([
 		call(LoggerSaga),
-		...[].map(fork)
+		...[LoginSaga].map(fork)
 	]);
 });
